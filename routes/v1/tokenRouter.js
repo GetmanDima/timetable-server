edf7a039f453958b82493f794857b67b6e3318ce
GetmanDima <dima.getman12112001@gmail.com>
2022-05-02 01:07:24 +0300

    Fix tokenRouter

--- routes/v1/tokenRouter.js
+++ routes/v1/tokenRouter.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const {body} = require("express-validator");
 const TokenController = require("../../controllers/TokenController");
+const handleValidationErrors = require("../../middleware/handleValidationErrors");
 
 const router = express.Router();
 
 router.post(
   '/',
-  body('refreshToken').notEmpty(),
+  body('refreshToken').isString().notEmpty(),
+  handleValidationErrors,
   TokenController.update(true)
 )
 
--- routes/v2/tokenRouter.js
+++ routes/v2/tokenRouter.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const TokenController = require("../../controllers/TokenController");
 const {cookie} = require("express-validator");
+const handleValidationErrors = require("../../middleware/handleValidationErrors");
 
 const router = express.Router();
 
 router.post(
   '/',
-  cookie('refreshToken').notEmpty(),
+  cookie('refreshToken').isString().notEmpty(),
+  handleValidationErrors,
   TokenController.update(false)
 )
 
